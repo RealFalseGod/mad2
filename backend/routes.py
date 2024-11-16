bdc3808e30506882cf710cd2df8cc041c766565a
pratyush <pratyushjdhv@gmail.com>
2024-11-16 07:16:02 +0000

    added login route

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,8 @@
-from flask import current_app as app
-from flask_security import auth_required
+from flask import current_app as app, request, jsonify
+from flask_security import auth_required, verify_password
+
+
+datastore = app.security.datastore
 
 
 @app.get("/")
@@ -12,3 +15,33 @@ def home():
 @auth_required()
 def protected():
     return "<h1>Admin Page</h1>"
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    email = data.get("eemail")
+    password = data.get("password")
+    address = data.get("address")
+    pincode = data.get("pincode")
+
+    if not email or not password:
+        return jsonify({"error": "Invalid email or password"}), 400
+
+    user = datastore.find_user(email=email)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    if verify_password(password, user.password):
+        return (
+            jsonify(
+                {
+                    "token": user.get_auth_token(),
+                    "email": user.email,
+                    "role": user.role,
+                    "id": user.id,
+                }
+            ),
+            200,
+        )
