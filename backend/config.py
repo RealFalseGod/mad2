fa906219740847438562e545a7e59f4149e42940
pratyush <pratyushjdhv@gmail.com>
2024-11-17 14:48:21 +0000

    minor changes but now working protected route

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class local(config):
     SECURITY_PASSWORD_HASH = "bcrypt"
     SECURITY_PASSWORD_SALT = "wowsecrettobypassattack"
     SECRET_KEY = "veryverysecretkey"
-    SECURITY_TOKEN_AUTHENTICATION_HEADER = "authentication_token"
+    SECURITY_TOKEN_AUTHENTICATION_HEADER = "auth-token"
 
 
     WTF_CSRF_ENABLED = False
