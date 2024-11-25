cc5fb1190142dd4216b8a02d720eb3e3837ef50b
pratyush <pratyushjdhv@gmail.com>
2024-11-25 10:53:53 +0000

    update Navbar component to conditionally render links based on user authentication and role

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -2,9 +2,12 @@ export default {
   template: `
     <div>
         <router-link to="/"> home </router-link>
-        <router-link to="/login"> login </router-link>
-        <router-link to="/register"> register </router-link>
-        <button class="btn btn-secondary" @click="$store.commit('logout')"> Logout </button>
+        <router-link v-if="!$store.state.loggedin" to="/login"> login </router-link>
+        <router-link v-if="!$store.state.loggedin" to="/register"> register </router-link>
+        <router-link v-if="$store.state.loggedin && $store.state.role == 'admin'" to="/admin-dashboard"> admin page </router-link>
+        <router-link v-if="$store.state.loggedin && $store.state.role == 'user'" to="/services"> services </router-link>
+        <button v-if="$store.state.loggedin" class="btn btn-secondary" @click="$store.commit('logout')"> Logout </button>
+
     </div>
     `,
 };
