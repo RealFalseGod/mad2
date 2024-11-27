bf83bbf137ab8b2d43f0f18b2bf9a42ca182a509
pratyush <pratyushjdhv@gmail.com>
2024-11-27 06:40:23 +0000

    working search bar in service list

--- frontend/pages/services_list.js
+++ frontend/pages/services_list.js
@@ -1,11 +1,12 @@
 import Postcard from "../components/Postcard.js";
 
 export default {
-  template: `
+    template: `
     <div class='p-4'>
         <h1>Services List</h1>
+        <input type="text" v-model="searchQuery" placeholder="Search services..." class="form-control mb-3" />
         <Postcard 
-            v-for="service in services" 
+            v-for="service in filteredServices" 
             :key="service.id" 
             :service='service.service' 
             :name='service.name' 
@@ -16,32 +17,40 @@ export default {
     </div>
     `,
 
-  data() {
-    return {
-      services: [],
-    };
-  },
-
-  methods: {
-    async fetchServices() {
-        const res = await fetch(location.origin + "/api/posts", {
-            headers: {
-                "auth-token": this.$store.state.auth_token,
-            },
-        });
-        this.services = await res.json();
+    data() {
+        return {
+            services: [],
+            searchQuery: "",
+        };
+    },
+    computed: {
+        filteredServices() {
+            return this.services.filter((service) =>
+                service.service.toLowerCase().includes(this.searchQuery.toLowerCase())
+            );
+        },
     },
 
-    deletepost(post_id) {
-        // Filter out the deleted post from the services array
-        this.services = this.services.filter(service => service.id !== post_id);
-    }
-  },
+    methods: {
+        async fetchServices() {
+            const res = await fetch(location.origin + "/api/posts", {
+                headers: {
+                    "auth-token": this.$store.state.auth_token,
+                },
+            });
+            this.services = await res.json();
+        },
 
-  async mounted() {
-    await this.fetchServices();
-  },
-  components: {
-    Postcard,
-  },
+        deletepost(post_id) {
+            // Filter out the deleted post from the services array
+            this.services = this.services.filter((service) => service.id !== post_id);
+        },
+    },
+
+    async mounted() {
+        await this.fetchServices();
+    },
+    components: {
+        Postcard,
+    },
 };
